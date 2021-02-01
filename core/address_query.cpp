cf442a855045c781ce1e56c89f8c0b6f1d1d2989
LeeOne1 <leeone1997@163.com>
2021-02-02 00:08:04 +0800

    update address_query

--- core/address_query.cpp
+++ core/address_query.cpp
@@ -75,7 +75,7 @@ ADDR_SEQ address_query::get_addr_seq(string btc_addr,ERROR_CODE *err){
     MYSQL_RES *result;
     MYSQL_FIELD *field;
 
-    if(!mysql_real_connect(&mysql, strconfig[0].c_str(), strconfig[1].c_str(), strconfig[2].c_str(), strconfig[3].c_str(), strconfig[4].c_str(), NULL, 0 ))
+    if(!mysql_real_connect(&mysql, strconfig[0].c_str(), strconfig[1].c_str(), strconfig[2].c_str(), strconfig[3].c_str(), atoi(strconfig[4].data()), NULL, 0 ))
     {
         return NULL_SEQ;
     }
@@ -113,7 +113,7 @@ ERROR_CODE address_query::get_btc_address(ADDR_SEQ seq,char *btc_addr){
     MYSQL_RES *result;
     MYSQL_FIELD *field;
 
-    if(!mysql_real_connect(&mysql, strconfig[0].c_str(), strconfig[1].c_str(), strconfig[2].c_str(), strconfig[3].c_str(), strconfig[4].c_str(), NULL, 0 ))
+    if(!mysql_real_connect(&mysql, strconfig[0].c_str(), strconfig[1].c_str(), strconfig[2].c_str(), strconfig[3].c_str(), atoi(strconfig[4].data()), NULL, 0 ))
     {
         return INVALID_ADDR_SEQ;
     }
@@ -195,66 +195,79 @@ void address_query::output_addr(ADDR_SEQ seq,ostream os,int config){
 
         switch (config)
         {
-        case 0:
-            os<<"seq:"<<ai->seq<<endl;
-            break;
-        case 1:
-            string address = ai->btc_address;
-            os<<"btc_address:"<<address<<endl;
-            break;
-        case 2:
-            os<<"type:"<<ai->type<<endl;
-            break;
-        case 3:
-            os<<"address_info2:"<<endl;
-            os<<" tran_num:"<<ai->info2.tran_num<<endl;
-            os<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            os<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            os<<" first_clock:"<<ai->info2.first_clock<<endl;
-            os<<" last_clock:"<<ai->info2.last_clock<<endl;
-            os<<" balance:"<<ai->info2.balance<<endl;
-            break;
-        case 4:
-            os<<"account_seq:"<<ai->account_seq<<endl;
-            break;
-        case 5:
-            os<<"valid:"<<ai->valid<<endl;
-            break;
-        
+        case 0:{
+                os<<"seq:"<<ai->seq<<endl;
+                break;
+            }
+        case 1:{
+                string address = ai->btc_address;
+                os<<"btc_address:"<<address<<endl;
+                break;
+            }
+        case 2:{
+                os<<"type:"<<ai->type<<endl;
+                break;
+            }
+        case 3:{
+                os<<"address_info2:"<<endl;
+                os<<" tran_num:"<<ai->info2.tran_num<<endl;
+                os<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                os<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                os<<" first_clock:"<<ai->info2.first_clock<<endl;
+                os<<" last_clock:"<<ai->info2.last_clock<<endl;
+                os<<" balance:"<<ai->info2.balance<<endl;
+                break;
+            }
+        case 4:{
+                os<<"account_seq:"<<ai->account_seq<<endl;
+                break;
+            }
+        case 5:{
+                os<<"valid:"<<ai->valid<<endl;
+                break;
+            }
+            
         //address_info2 
-        case 6:
-            os<<"tran_num:"<<ai->info2.tran_num<<endl;
-            break;
-        case 7:
-            os<<"first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            break;
-        case 8:
-            os<<"last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            break;
-        case 9:
-            os<<"first_clock:"<<ai->info2.first_clock<<endl;
-            break;
-        case 10:
-            os<<"last_clock:"<<ai->info2.last_clock<<endl;
-            break;
-        case 11:
-            os<<"balance:"<<ai->info2.balance<<endl;
-            break;
-        default:
-            os<<"seq:"<<ai->seq<<endl;
-            string address = ai->btc_address;
-            os<<"btc_address:"<<address<<endl;
-            os<<"type:"<<ai->type<<endl;
-            os<<"address_info2:"<<endl;
-            os<<" tran_num:"<<ai->info2.tran_num<<endl;
-            os<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            os<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            os<<" first_clock:"<<ai->info2.first_clock<<endl;
-            os<<" last_clock:"<<ai->info2.last_clock<<endl;
-            os<<" balance:"<<ai->info2.balance<<endl;
-            os<<"account_seq:"<<ai->account_seq<<endl;
-            os<<"valid:"<<ai->valid<<endl;
-            break;
+        case 6:{
+                os<<"tran_num:"<<ai->info2.tran_num<<endl;
+                break;
+            }
+        case 7:{
+                os<<"first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                break;
+            }
+        case 8:{
+                os<<"last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                break;
+            }
+        case 9:{
+                os<<"first_clock:"<<ai->info2.first_clock<<endl;
+                break;
+            }
+        case 10:{
+                os<<"last_clock:"<<ai->info2.last_clock<<endl;
+                break;
+            }
+        case 11:{
+                os<<"balance:"<<ai->info2.balance<<endl;
+                break;
+            }
+        default:{
+                os<<"seq:"<<ai->seq<<endl;
+                string address = ai->btc_address;
+                os<<"btc_address:"<<address<<endl;
+                os<<"type:"<<ai->type<<endl;
+                os<<"address_info2:"<<endl;
+                os<<" tran_num:"<<ai->info2.tran_num<<endl;
+                os<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                os<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                os<<" first_clock:"<<ai->info2.first_clock<<endl;
+                os<<" last_clock:"<<ai->info2.last_clock<<endl;
+                os<<" balance:"<<ai->info2.balance<<endl;
+                os<<"account_seq:"<<ai->account_seq<<endl;
+                os<<"valid:"<<ai->valid<<endl;
+                break;
+            }
         }
     }
     return;
@@ -288,66 +301,79 @@ void address_query::output_addr(ADDR_SEQ seq,int config){
 
         switch (config)
         {
-        case 0:
-            cout<<"seq:"<<ai->seq<<endl;
-            break;
-        case 1:
-            string address = ai->btc_address;
-            cout<<"btc_address:"<<address<<endl;
-            break;
-        case 2:
-            cout<<"type:"<<ai->type<<endl;
-            break;
-        case 3:
-            cout<<"address_info2:"<<endl;
-            cout<<" tran_num:"<<ai->info2.tran_num<<endl;
-            cout<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            cout<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            cout<<" first_clock:"<<ai->info2.first_clock<<endl;
-            cout<<" last_clock:"<<ai->info2.last_clock<<endl;
-            cout<<" balance:"<<ai->info2.balance<<endl;
-            break;
-        case 4:
-            cout<<"account_seq:"<<ai->account_seq<<endl;
-            break;
-        case 5:
-            cout<<"valid:"<<ai->valid<<endl;
-            break;
+        case 0:{
+                cout<<"seq:"<<ai->seq<<endl;
+                break;
+            }
+        case 1:{
+                string address = ai->btc_address;
+                cout<<"btc_address:"<<address<<endl;
+                break;
+            }
+        case 2:{
+                cout<<"type:"<<ai->type<<endl;
+                break;
+            }
+        case 3:{
+                cout<<"address_info2:"<<endl;
+                cout<<" tran_num:"<<ai->info2.tran_num<<endl;
+                cout<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                cout<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                cout<<" first_clock:"<<ai->info2.first_clock<<endl;
+                cout<<" last_clock:"<<ai->info2.last_clock<<endl;
+                cout<<" balance:"<<ai->info2.balance<<endl;
+                break;
+            }
+        case 4:{
+                cout<<"account_seq:"<<ai->account_seq<<endl;
+                break;
+            }
+        case 5:{
+                cout<<"valid:"<<ai->valid<<endl;
+                break;
+            }
         
         //address_info2 
-        case 6:
-            cout<<"tran_num:"<<ai->info2.tran_num<<endl;
-            break;
-        case 7:
-            cout<<"first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            break;
-        case 8:
-            cout<<"last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            break;
-        case 9:
-            cout<<"first_clock:"<<ai->info2.first_clock<<endl;
-            break;
-        case 10:
-            cout<<"last_clock:"<<ai->info2.last_clock<<endl;
-            break;
-        case 11:
-            cout<<"balance:"<<ai->info2.balance<<endl;
-            break;
-        default:
-            cout<<"seq:"<<ai->seq<<endl;
-            string address = ai->btc_address;
-            cout<<"btc_address:"<<address<<endl;
-            cout<<"type:"<<ai->type<<endl;
-            cout<<"address_info2:"<<endl;
-            cout<<" tran_num:"<<ai->info2.tran_num<<endl;
-            cout<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
-            cout<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
-            cout<<" first_clock:"<<ai->info2.first_clock<<endl;
-            cout<<" last_clock:"<<ai->info2.last_clock<<endl;
-            cout<<" balance:"<<ai->info2.balance<<endl;
-            cout<<"account_seq:"<<ai->account_seq<<endl;
-            cout<<"valid:"<<ai->valid<<endl;
-            break;
+        case 6:{
+                cout<<"tran_num:"<<ai->info2.tran_num<<endl;
+                break;
+            }
+        case 7:{
+                cout<<"first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                break;
+            }
+        case 8:{
+                cout<<"last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                break;
+            }
+        case 9:{
+                cout<<"first_clock:"<<ai->info2.first_clock<<endl;
+                break;
+            }
+        case 10:{
+                cout<<"last_clock:"<<ai->info2.last_clock<<endl;
+                break;
+            }
+        case 11:{
+                cout<<"balance:"<<ai->info2.balance<<endl;
+                break;
+            }
+        default:{
+                cout<<"seq:"<<ai->seq<<endl;
+                string address = ai->btc_address;
+                cout<<"btc_address:"<<address<<endl;
+                cout<<"type:"<<ai->type<<endl;
+                cout<<"address_info2:"<<endl;
+                cout<<" tran_num:"<<ai->info2.tran_num<<endl;
+                cout<<" first_block_seq:"<<ai->info2.first_block_seq<<endl;
+                cout<<" last_block_seq:"<<ai->info2.last_block_seq<<endl;
+                cout<<" first_clock:"<<ai->info2.first_clock<<endl;
+                cout<<" last_clock:"<<ai->info2.last_clock<<endl;
+                cout<<" balance:"<<ai->info2.balance<<endl;
+                cout<<"account_seq:"<<ai->account_seq<<endl;
+                cout<<"valid:"<<ai->valid<<endl;
+                break;
+            }
         }
     }
     return;
