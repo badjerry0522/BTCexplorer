e95e0d654d223d65c88b45e1e991035022057141
LeftEarEast <635774040@qq.com>
2021-06-13 00:15:39 +0800

    修改chip_mix app

--- app/chip_mix.cpp
+++ app/chip_mix.cpp
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #include "../include/core_type.h"
 #include "../include/trans_file.h"
 #include "../include/app_manager.h"
@@ -71,22 +72,35 @@ void savedata2(map<ADDR_SEQ, int64_t> dataforpool, string outfile) {
 	}
 	outFile3.close();
 }
-
+void savedata3(map<ADDR_SEQ, int> dataforpool, string outfile) {
+	ofstream outFile3;
+	outFile3.open(outfile.data(), ios::out);
+	map<ADDR_SEQ, int>::iterator iter;
+	for (iter = dataforpool.begin(); iter != dataforpool.end(); iter++) {
+		outFile3 << iter->first << " " << iter->second;
+		outFile3 << endl;
+	}
+	outFile3.close();
+}
 
 void getMixPool(trans_in_mem* tim, address_query* addrq) {
 	float time_use = 0;
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
 	ofstream outFile1;
-	outFile1.open("chaintx1.txt", ios::out);
+	outFile1.open("power2_pools_I.txt", ios::out);
 
 	ofstream outFile2;
-	outFile2.open("chaintx2.txt", ios::out);
+	outFile2.open("power2_pools_II.txt", ios::out);
+
 
 	map<int64_t, map<int, int> > dataforpool1;
 	map<int64_t, map<int, int> > dataforpool2;
 	map<ADDR_SEQ, int64_t> addresses;
+	map<ADDR_SEQ, int> pools_tran;
 
+	int chip_I = 0;
+	int chip_II = 0;
 
 	ERROR_CODE err;
 	struct transaction_binary *tp = (struct transaction_binary *)malloc(sizeof(struct transaction_binary));
@@ -96,10 +110,11 @@ void getMixPool(trans_in_mem* tim, address_query* addrq) {
 
 		CLOCK blocktime = tim->find_block_time(i, &err);
 
-		if (blocktime >= 1610239070 || blocktime <= 1577808000) continue;
+		//if (blocktime >= 1610239070 || blocktime <= 1577808000) continue;
 		
 		int output_num = tim->get_output_num(i, &err);
-
+		printf("%d----", i);
+		printf("\r\033[k");
 
 		if (output_num > 1) {
 			int ispool = 0;
@@ -128,6 +143,7 @@ void getMixPool(trans_in_mem* tim, address_query* addrq) {
 				
 				int input_num = tim->get_input_num(i, &err);
 				map<ADDR_SEQ, int64_t>::iterator l_it;
+				
 
 				//I
 				if (vset.size() == 1) {
@@ -149,10 +165,13 @@ void getMixPool(trans_in_mem* tim, address_query* addrq) {
 						outFile1 << addr << " " << ((double)tp->outputs[index].bitcoin / 100000000) << endl;
 
 						l_it = addresses.find(tp->outputs[index].addr);
-						if(l_it == addresses.end()) addresses.insert(map<ADDR_SEQ, int64_t>::value_type(tp->outputs[index].addr, tp->outputs[index].bitcoin));
-
+						if (l_it == addresses.end()) {
+							addresses.insert(map<ADDR_SEQ, int64_t>::value_type(tp->outputs[index].addr, tp->outputs[index].bitcoin));
+							pools_tran.insert(map<ADDR_SEQ, int>::value_type(tp->outputs[index].addr, i));
+						}
 					}
 					outFile1 << endl;
+					chip_I++;
 
 					// Statistics pool
 					map<int64_t, map<int, int> >::iterator iteration;
@@ -201,10 +220,14 @@ void getMixPool(trans_in_mem* tim, address_query* addrq) {
 						outFile2 << addr << " " << ((double)tp->outputs[index].bitcoin / 100000000) << endl;
 
 						l_it = addresses.find(tp->outputs[index].addr);
-						if (l_it == addresses.end()) addresses.insert(map<ADDR_SEQ, int64_t>::value_type(tp->outputs[index].addr, tp->outputs[index].bitcoin));
+						if (l_it == addresses.end()) { 
+							addresses.insert(map<ADDR_SEQ, int64_t>::value_type(tp->outputs[index].addr, tp->outputs[index].bitcoin)); 
+							pools_tran.insert(map<ADDR_SEQ, int>::value_type(tp->outputs[index].addr, i));
+
+						}
 					}
 					outFile2 << endl;
-
+					chip_II++;
 
 
 						// Statistics pool
@@ -243,20 +266,23 @@ void getMixPool(trans_in_mem* tim, address_query* addrq) {
 	}
 	
 	cout << "Save data ............." << endl;
-	savedata(dataforpool1, "chaintxdata1.txt");
-	savedata(dataforpool2, "chaintxdata2.txt");
-	savedata2(addresses, "chaintxaddress2020.txt");
+	savedata(dataforpool1, "power2_pools_I_data.txt");
+	savedata(dataforpool2, "power2_pools_II_data.txt");
+	savedata2(addresses, "chaintxaddress.txt");
+	savedata3(pools_tran, "pools_trans.txt");
 
 	gettimeofday(&end, NULL);
 	time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 	cout << "get mix pool time use=" << time_use / 1000000 << "s" << endl << endl;
-
+	cout << "I transactions: " << chip_I << endl;
+	cout << "II transactions: " << chip_II << endl;
 
 	outFile1.close();
 	outFile2.close();
 	dataforpool1.clear();
 	dataforpool2.clear();
 	addresses.clear();
+	pools_tran.clear();
 	free(tp);
 }
 
@@ -269,26 +295,27 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 
 	//I chipMix output transaction
 	ofstream exitTran1;
-	exitTran1.open("ChipMix_2020_exitTran_I.txt", ios::out);
+	exitTran1.open("ChipMix_exitTran_I.txt", ios::out);
 
 	//II ChipMix output transaction
 	ofstream exitTran2;
-	exitTran2.open("ChipMix_2020_exitTran_II.txt", ios::out);
+	exitTran2.open("ChipMix_exitTran_II.txt", ios::out);
 
 	//II ChipMix input address not belong to pool and its output value
 	ofstream exitOdd;
-	exitOdd.open("ChipMix_2020_exitOdd_addr.txt", ios::out);
+	exitOdd.open("ChipMix_exitOdd_addr.txt", ios::out);
 
 	//I and II ChipMix output address and value 
 	ofstream dest;
-	dest.open("ChipMix_2020_dest_addr.txt", ios::out);
+	dest.open("ChipMix_dest_addr.txt", ios::out);
 
-	string csvpath = "chaintxaddress2020.txt";
+
+
+	string csvpath = "chaintxaddress.txt";
 	map<ADDR_SEQ, int64_t> addresses;//map
 	if (!csvpath.empty()) {
 		ifstream fp(csvpath.data());
 		string line;
-
 		
 		while (getline(fp, line)) {
 
@@ -306,6 +333,8 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 	}
 	cout << "addresses:" << addresses.size() << endl;
 
+	int chip_I = 0;
+	int chip_II = 0;
 
 
 	ERROR_CODE err;
@@ -314,8 +343,11 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 	for (int i = 0; i < max_seq; i++) {
 
 		CLOCK blocktime = tim->find_block_time(i,&err);
-		if (blocktime >= 1610239070 || blocktime <= 1577808000) continue;
+		//if (blocktime >= 1610239070 || blocktime <= 1577808000) continue;
 		int output_num = tim->get_output_num(i,&err);
+
+		printf("%d----", i);
+		printf("\r\033[k");
 
 		if (output_num == 1) {
 			int input_num = tim->get_input_num(i,&err);
@@ -355,6 +387,7 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 			//I chipMix
 			if (ischip == input_num && ischip > 0)
 			{
+				chip_I++;
 				//Ê±¼ä
 				exitTran1 << stamp_to_standard(blocktime) << endl;
 				dest << stamp_to_standard(blocktime) << endl;
@@ -379,11 +412,13 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 
 				exitTran1 << endl;
 				dest << endl;
+			
 			}
 
 			//II ChipMix
 			if (ischip == (input_num - 1) && ischip > 0 && II == true)
 			{
+				chip_II++;
 				//time
 				exitTran2 << stamp_to_standard(blocktime) << endl;
 				dest << stamp_to_standard(blocktime) << endl;
@@ -424,15 +459,20 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 	dest.close();
 
 	ofstream unused;
-	unused.open("ChipMix_2020_unused.txt", ios::out);
+	unused.open("ChipMix_unused.txt", ios::out);
+	ofstream used;
+	used.open("ChipMix_used.txt", ios::out);
+
 	map<ADDR_SEQ, int64_t>::iterator iter;
 	for (iter = addresses.begin(); iter != addresses.end(); iter++) {
-		if (iter->second != -10)
-		{
+		if (iter->second != -10){
 			char btc_addr[256];
 			err = addrq->get_btc_address(iter->first, btc_addr);
 			string addr = btc_addr;
 			unused << addr << " " << iter->second << endl;
+		}
+		else{
+			used << iter->first << endl;
 		}
 	}
 	unused.close();
@@ -442,8 +482,233 @@ void ChipMix(trans_in_mem* tim,address_query* addrq) {
 	gettimeofday(&end, NULL);
 	time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 	cout << "chipmix time use=" << time_use / 1000000 << "s" << endl << endl;
+	cout << "chipmix I transactions: " << chip_I << endl;
+	cout << "chipmix II transactions: " << chip_II << endl;
 
 	delete tp;
+}
+
+void getChipMixPool(trans_in_mem* tim, address_query* addrq) {
+
+	float time_use = 0;
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	ofstream outFile1;
+	outFile1.open("ChipMix_pools_I.txt", ios::out);
+
+	ofstream outFile2;
+	outFile2.open("ChipMix_pools_II.txt", ios::out);
+
+
+
+	string csvpath = "pools_trans.txt";
+	map<ADDR_SEQ, int> addr_tran;//map
+	if (!csvpath.empty()) {
+		ifstream fp(csvpath.data());
+		string line;
+
+		while (getline(fp, line)) {
+
+			string idstr;
+			string vs;
+			istringstream readstr(line);
+			//split
+			getline(readstr, idstr, ' '); //id
+			getline(readstr, vs, ' '); //value
+			int vv = atoll(vs.c_str());
+			int id = atoll(idstr.c_str());
+			if (id < 3) continue; //error address
+			addr_tran.insert(map<ADDR_SEQ, int>::value_type((ADDR_SEQ)id, vv));
+		}
+		fp.close();
+	}
+	
+
+	set<int> trans;
+	set<int>::iterator s_iter;
+	map<int64_t, map<int, int> > dataforpool1;
+	map<int64_t, map<int, int> > dataforpool2;
+	csvpath = "ChipMix_used.txt";
+	if (!csvpath.empty()) {
+		ifstream fp(csvpath.data());
+		string line;
+
+		while (getline(fp, line)) {
+			int id = atoll(line.c_str());
+			trans.insert(addr_tran[(ADDR_SEQ)id]);
+		}
+		fp.close();
+	}
+	cout << "used trans:" << trans.size() << endl;
+
+	ERROR_CODE err;
+	struct transaction_binary *tp = (struct transaction_binary *)malloc(sizeof(struct transaction_binary));
+	int chip_I = 0;
+	int chip_II = 0;
+
+	for (s_iter = trans.begin(); s_iter != trans.end(); s_iter++)
+	{
+
+		CLOCK blocktime = tim->find_block_time(*s_iter, &err);
+
+		//if (blocktime >= 1610239070 || blocktime <= 1577808000) continue;
+
+		int output_num = tim->get_output_num(*s_iter, &err);
+		printf("%d----", *s_iter);
+		printf("\r\033[k");
+
+
+		int ispool = 0;
+		set<LONG_BTC_VOL> vset;
+		int64_t poolvalue = 0;
+
+		tim->get_tran_binary(*s_iter, tp);
+		for (unsigned int index = 0; index < output_num; index++) {
+
+			LONG_BTC_VOL outvalue = tp->outputs[index].bitcoin;
+			vset.insert(outvalue);
+			if (vset.size() > 2) {
+				// issave = false;
+				vset.clear();
+				break;
+			}
+			if (outvalue != 0 && outvalue % 100000 == 0 && isPowerOfTwo((int64_t)(outvalue / 100000)) != 0) {
+				ispool++;
+				poolvalue = outvalue;
+			}
+		}
+
+		if (ispool == output_num || (ispool >= 2 && ispool == (output_num - 1))) {
+
+			int input_num = tim->get_input_num(*s_iter, &err);
+			
+
+
+			//I
+			if (vset.size() == 1) {
+
+				outFile1 << stamp_to_standard(blocktime) << endl;
+				for (unsigned int index = 0; index < input_num; index++) {
+
+					char btc_addr[256];
+					err = addrq->get_btc_address(tp->inputs[index].addr, btc_addr);
+					string addr = btc_addr;
+					outFile1 << addr << " " << ((double)tp->inputs[index].bitcoin / 100000000) << endl;
+				}
+				outFile1 << "TO" << endl;
+				for (unsigned int index = 0; index < output_num; index++) {
+
+					char btc_addr[256];
+					err = addrq->get_btc_address(tp->outputs[index].addr, btc_addr);
+					string addr = btc_addr;
+					outFile1 << addr << " " << ((double)tp->outputs[index].bitcoin / 100000000) << endl;
+				}
+				outFile1 << endl;
+				chip_I++;
+
+				// Statistics pool
+				map<int64_t, map<int, int> >::iterator iteration;
+				iteration = dataforpool1.find(poolvalue);
+				int size = output_num;
+				if (dataforpool1.size() == 0 || iteration == dataforpool1.end()) {
+					map<int, int> temp;
+					temp.insert(map<int, int>::value_type(size, 1));
+					dataforpool1.insert(map<int64_t, map<int, int> >::value_type(poolvalue, temp));
+				}
+				else {
+					map<int, int>::iterator seiteration;
+					seiteration = iteration->second.find(size);
+					if (iteration->second.size() == 0 || seiteration == iteration->second.end()) {
+						iteration->second.insert(map<int, int>::value_type(size, 1));
+					}
+					else
+					{
+						seiteration->second = seiteration->second + 1;
+						seiteration = iteration->second.find(-1); //max
+						seiteration->second = (seiteration->second > size ? seiteration->second : size);
+						seiteration = iteration->second.find(-2); //min
+						seiteration->second = (seiteration->second < size ? seiteration->second : size);
+					}
+				}
+
+
+			}
+			//II
+			if (vset.size() == 2) {
+				outFile2 << stamp_to_standard(blocktime) << endl;
+				for (unsigned int index = 0; index < input_num; index++) {
+
+					char btc_addr[256];
+					err = addrq->get_btc_address(tp->inputs[index].addr, btc_addr);
+					string addr = btc_addr;
+					outFile2 << addr << " " << ((double)tp->inputs[index].bitcoin / 100000000) << endl;
+
+				}
+				outFile2 << "TO" << endl;
+				for (unsigned int index = 0; index < output_num; index++) {
+
+					char btc_addr[256];
+					err = addrq->get_btc_address(tp->outputs[index].addr, btc_addr);
+					string addr = btc_addr;
+					outFile2 << addr << " " << ((double)tp->outputs[index].bitcoin / 100000000) << endl;
+
+				}
+				outFile2 << endl;
+				chip_II++;
+
+
+				// Statistics pool
+				map<int64_t, map<int, int> >::iterator iteration;
+				iteration = dataforpool2.find(poolvalue);
+				int size = output_num;
+
+
+				if (dataforpool2.size() == 0 || iteration == dataforpool2.end()) {
+					map<int, int> temp;
+					temp.insert(map<int, int>::value_type(size, 1));
+					dataforpool2.insert(map<int64_t, map<int, int> >::value_type(poolvalue, temp));
+				}
+				else {
+					map<int, int>::iterator seiteration;
+					seiteration = iteration->second.find(size);
+					if (iteration->second.size() == 0 || seiteration == iteration->second.end()) {
+						iteration->second.insert(map<int, int>::value_type(size, 1));
+					}
+					else
+					{
+						seiteration->second = seiteration->second + 1;
+						seiteration = iteration->second.find(-1); //max
+						seiteration->second = (seiteration->second > size ? seiteration->second : size);
+						seiteration = iteration->second.find(-2); //min
+						seiteration->second = (seiteration->second < size ? seiteration->second : size);
+					}
+				}
+
+
+
+			}
+			vset.clear();
+
+
+		}	
+	}
+	savedata(dataforpool1, "ChipMix_pools_I_data.txt");
+	savedata(dataforpool2, "ChipMix_pools_II_data.txt");
+	gettimeofday(&end, NULL);
+	time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+	cout << "get chipmix transactions time use=" << time_use / 1000000 << "s" << endl << endl;
+	cout << "I transactions: " << chip_I << endl;
+	cout << "II transactions: " << chip_II << endl;
+
+
+
+	outFile1.close();
+	outFile2.close();
+	dataforpool1.clear();
+	dataforpool2.clear();
+	addr_tran.clear();
+	trans.clear();
+	free(tp);
 }
 
 ERROR_CODE chipmix_app(int app_argn, void **argv) {
@@ -455,7 +720,7 @@ ERROR_CODE chipmix_app(int app_argn, void **argv) {
 	
 	getMixPool(tim, addrq);
 	ChipMix(tim,addrq);
-
+	getChipMixPool(tim,addrq);
 
 	return NO_ERROR;
 }
